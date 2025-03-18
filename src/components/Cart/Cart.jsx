5415318525e13a9f5d7aeb83416d0fd07d4525ab
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-18 17:19:33 +0530

    ✨ update dynamically render items and total amount from Redux state...

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,20 +5,41 @@ import { useSelector } from "react-redux";
 
 const Cart = () => {
    const showCart = useSelector((state) => state.cart.showCart);
+   const cartItems = useSelector((state) => state.cartOperation.items);
+   const totalAmount = useSelector((state) => state.cartOperation.totalAmount);
    return (
       showCart && (
          <Card className={classes.cart}>
             <h2>Your Shopping Cart</h2>
             <ul>
-               <CartItem
-                  item={{
-                     title: "Test Item",
-                     quantity: 3,
-                     total: 18,
-                     price: 6,
-                  }}
-               />
+               {cartItems.map((item) => (
+                  <CartItem
+                     key={item.id}
+                     item={{
+                        title: item.title,
+                        quantity: item.quantity,
+                        total: item.totalPrice,
+                        price: item.price,
+                     }}
+                  />
+               ))}
             </ul>
+            <div
+               className={classes.total}
+               style={{
+                  fontWeight: "bold",
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  alignItems: "center",
+               }}
+            >
+               <span style={{ fontSize: "1.2rem", marginRight: "10px" }}>
+                  Total Amount:{" "}
+               </span>
+               <span style={{ fontSize: "1.2rem" }}>
+                  ${totalAmount.toFixed(2)}
+               </span>
+            </div>
          </Card>
       )
    );
